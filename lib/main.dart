e40e2c308e646d80122c89e999c4821d1acd4c50
samuel <samuelsinaga53@gmail.com>
2025-03-17 10:45:00 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,121 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-/// Flutter code sample for [Card].
+void main() => runApp(const MyApp());
 
-void main() {
-  runApp(const CardExamplesApp());
-}
-
-class CardExamplesApp extends StatelessWidget {
-  const CardExamplesApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text('Esia Friends')),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Card(child: _SampleCard(cardName: 'Elevated Card')),
-              Card.filled(child: _SampleCard(cardName: 'Filled Card')),
-              Card.outlined(child: _SampleCard(cardName: 'Outlined Card')),
-            ],
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Text(
+            "Esia Friend",
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blueAccent, Colors.deepPurple],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                buildCustomContainer(
+                  "Nur Adam Mahfudh",
+                  Colors.orange,
+                  -0.07,
+                  0,
+                ),
+                buildCustomContainer(
+                  "Samuel Nardus H. Sinaga",
+                  Colors.green,
+                  0.07,
+                  1,
+                ),
+                buildCustomContainer("Agung Sedayu", Colors.red, -0.07, 2),
+                buildCustomContainer("Farhan Ramadhan", Colors.teal, 0.07, 3),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
-}
 
-class _SampleCard extends StatelessWidget {
-  const _SampleCard({required this.cardName});
-  final String cardName;
+  Widget buildCustomContainer(
+    String text,
+    Color color,
+    double angle,
+    int shapeType,
+  ) {
+    ShapeBorder shape;
+    switch (shapeType) {
+      case 0:
+        shape = const BeveledRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        );
+        break;
+      case 1:
+        shape = RoundedRectangleBorder(borderRadius: BorderRadius.circular(15));
+        break;
+      case 2:
+        shape = const StadiumBorder();
+        break;
+      case 3:
+        shape = const ContinuousRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            bottomRight: Radius.circular(50),
+          ),
+        );
+        break;
+      default:
+        shape = RoundedRectangleBorder(borderRadius: BorderRadius.circular(15));
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(width: 300, height: 100, child: Center(child: Text(cardName)));
+    return Transform.rotate(
+      angle: angle * math.pi,
+      child: Container(
+        height: 85,
+        width: 330,
+        alignment: Alignment.center,
+        margin: const EdgeInsets.symmetric(vertical: 12),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+        decoration: ShapeDecoration(
+          color: color,
+          shape: shape,
+          shadows: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 6,
+              offset: const Offset(2, 4),
+            ),
+          ],
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
   }
 }
