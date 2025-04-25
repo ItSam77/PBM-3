35a2cdff1bbc0012fdb1960e5a2549df06dfc4b8
samuel <samuelsinaga53@gmail.com>
2025-04-25 21:13:16 +0700

    added supabase flutter

---
+++ lib/supabase_service.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseService {
+  final supabase = Supabase.instance.client;
+
+  Future<String?> getBioById(int id) async {
+    final data = await supabase
+        .from('biodata')
+        .select('bio')
+        .eq('id', id)
+        .maybeSingle();
+
+    return data?['bio'];
+  }
+}
